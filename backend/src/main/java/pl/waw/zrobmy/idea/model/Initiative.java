b31ab406c98523830705e946fb7763b5b688e881
Konrad Starzyk <konrad.starzyk@gmail.com>
2017-10-14 16:17:04 +0200

    Add initiative properties

--- backend/src/main/java/pl/waw/zrobmy/idea/model/Initiative.java
+++ backend/src/main/java/pl/waw/zrobmy/idea/model/Initiative.java
@@ -1,10 +1,10 @@
 package pl.waw.zrobmy.idea.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by konrad on 14.10.2017.
@@ -14,11 +14,28 @@ import java.io.Serializable;
 public class Initiative implements Serializable {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 
-	@Column(name = "title")
-	private String title;
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "location")
+	private String location;
+
+	@JsonFormat(pattern = "YYYYMMDD")
+	@Column(name = "time")
+	private Date time;
+
+	@Column(name = "author")
+	private String author;
+
+	@Column(name = "fb_event")
+	private String fbEvent;
 
 	public Long getId() {
 		return id;
@@ -26,5 +43,54 @@ public class Initiative implements Serializable {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getFbEvent() {
+		return fbEvent;
+	}
+
+	public void setFbEvent(String fbEvent) {
+		this.fbEvent = fbEvent;
 	}
 }
