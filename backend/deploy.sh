293c010d10ee9f6c9f9fc34690bbc84183c82b02
Konrad Starzyk <konrad.starzyk@gmail.com>
2017-10-14 16:17:41 +0200

    Update deployment script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,3 +2,6 @@
 
 mvn clean package
 scp target/zrobmy-0.0.1-SNAPSHOT.jar root@zrobmy.waw.pl:/opt/zrobmyapp
+ssh root@zrobmy.waw.pl 'cd /opt/zrobmyapp && kill $(cat ./pid.file)'
+ssh root@zrobmy.waw.pl 'cd /opt/zrobmyapp && java -jar zrobmy-0.0.1.SNAPSHOT.jar & echo $! > ./pid.file &'
+
