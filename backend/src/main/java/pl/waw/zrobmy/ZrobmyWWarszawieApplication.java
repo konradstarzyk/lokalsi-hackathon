bfb93d761018dd4cf7de23edf7c2825f6f51fed5
Konrad Starzyk <konrad.starzyk@gmail.com>
2017-10-14 12:42:34 +0200

    Initialize Spring Boot app

---
+++ backend/src/main/java/pl/waw/zrobmy/ZrobmyWWarszawieApplication.java
@@ -0,0 +1,12 @@
+package pl.waw.zrobmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZrobmyWWarszawieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZrobmyWWarszawieApplication.class, args);
+	}
+}
