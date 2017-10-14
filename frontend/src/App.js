f1a8aabe0aaa187673a02147888d8a1937765938
Monika Glier <monikaglier@gmail.com>
2017-10-14 15:23:17 +0200

    Update mocks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Jumbotron } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 import './App.css'
 
 
@@ -16,21 +16,33 @@ class App extends Component {
   componentDidMount() {
     const initiatives = [
       { name: 'Lodowisko',
+        description: 'Zróbmy razem lodowisko dla dzieciaków',
         location: 'Warszawa Ursynów',
-        time: 'Jutro',
-        author: 'Łyżwa' },
+        time: '28.12.2017',
+        author: 'Andrzej',
+        event: 'www.facebook.com',
+      },
       { name: 'Piaskownica',
+        description: 'Przy ulicy Kubusia Puchatka lezy sporo piasku, może zrobilibyśmy z tego piaskownicę?',
         location: 'Warszawa Mokotów',
-        time: 'Jutro',
-        author: 'Zamek z piasku' },
+        time: 'Maj 2018',
+        author: 'Aneta',
+        event: 'www.facebook.com',
+      },
       { name: 'Plac zabaw',
+        description: 'Posiadam sporo drewnianych elementów, z których można by zrobić plac zabaw dla dzieciaków. Zapraszam do kontaktu.',
         location: 'Warszawa Targówek',
-        time: 'Czerwiec',
-        author: 'Zjeżdżalnia' },
+        time: 'Czerwiec 2018',
+        author: 'Tomek',
+        event: 'www.facebook.com',
+      },
       { name: 'Kino letnie',
+        description: 'W naszym podwórku przy ulicy Kinowej można by wyświetlać filmy w ramach sąsiedzkiego kina letniego. Jedna ściana idealnie się do tego nadaje. Zapraszam do wspólnej inicjatywy.',
         location: 'Warszawa Bemowo',
-        time: 'Sierpień',
-        author: 'Thriller' },
+        time: 'Sierpień 2018',
+        author: 'Staszek',
+        event: 'www.facebook.com',
+      },
     ]
 
     this.setState({ initiatives })
@@ -39,12 +51,12 @@ class App extends Component {
   render() {
     return (
       <span>
-      <Jumbotron className='App-intro'>
-        <header>
-          <h1 className="appTitle">zróbmy.waw.pl</h1>
-        </header>
-      </Jumbotron>
-      <InitiativesList initiatives={this.state.initiatives} />
+        <Jumbotron className='App-intro'>
+          <header>
+            <h1 className="appTitle">zróbmy.waw.pl</h1>
+          </header>
+        </Jumbotron>
+        <InitiativesList initiatives={this.state.initiatives} />
       </span>
     );
   }
--- frontend/src/modules/InitiativesList/Initiative/index.jsx
+++ frontend/src/modules/InitiativesList/Initiative/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ListGroupItem } from 'react-bootstrap'
+import { Button, ListGroupItem, Modal } from 'react-bootstrap'
 
 class Initiative extends Component {
 
@@ -12,23 +12,42 @@ class Initiative extends Component {
     this.setState({ showDetails: true })
   }
 
-  hideItem() {
+  closeItem() {
     this.setState({ showDetails: false })
+  }
+
+  renderModal() {
+    const { item } = this.props
+    return <Modal.Dialog>
+            <Modal.Header>
+              <Modal.Title>{item.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>Lokalizacja: {item.location}</p>
+              <p>Kiedy? {item.time}</p>
+              <p>Autor: {item.author}</p>
+              <p>Opis: {item.description}</p>
+              <a href={item.event}>Link do wydarzenia</a>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => this.closeItem()}>Close</Button>
+            </Modal.Footer>
+           </Modal.Dialog>
   }
 
   render() {
     const { item } = this.props
-
     return (
       <div>
         <ListGroupItem
           header={item.name}
           onClick={() => this.showItem()}
         >
-          <div>{item.location}</div>
-          <div>{item.time}</div>
-          <div>{item.author}</div>
+          {item.location}
+          {item.time}
+          {item.author}
         </ListGroupItem>
+        {this.state.showDetails && this.renderModal()}
       </div>
     )
   }
