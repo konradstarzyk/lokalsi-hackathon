64bbcfcdd71d63ef5f0e8b32fd76ca5feae46b0f
Konrad Starzyk <konrad.starzyk@gmail.com>
2017-10-14 19:55:28 +0200

    Rename Zrobmy w Warszawie -> Lokalsi

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 tar -zcvf frontend.tar.gz -C build .
-scp frontend.tar.gz root@zrobmy.waw.pl:/var/www/html
-ssh root@207.154.228.15 'cd /var/www/html && tar -zxvf frontend.tar.gz'
+scp frontend.tar.gz root@lokalsi.waw.pl:/var/www/html
+ssh root@lokalsi.waw.pl 'cd /var/www/html && tar -zxvf frontend.tar.gz'
