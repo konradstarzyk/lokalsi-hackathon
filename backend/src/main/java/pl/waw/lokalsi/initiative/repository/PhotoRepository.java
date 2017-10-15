ec72d80630eda8f94d95794e77f51853fad5deab
Konrad Starzyk <konrad.starzyk@gmail.com>
2017-10-15 11:26:35 +0200

    Add photo uploading

--- backend/src/main/java/pl/waw/lokalsi/initiative/repository/PhotoRepository.java
+++ backend/src/main/java/pl/waw/lokalsi/initiative/repository/PhotoRepository.java
@@ -4,10 +4,11 @@ package pl.waw.lokalsi.initiative.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.waw.lokalsi.initiative.model.Initiative;
+import pl.waw.lokalsi.initiative.model.Photo;
 
 /**
  * Created by konrad on 14.10.2017.
  */
 @Repository
-public interface IdeaRepository extends JpaRepository<Initiative, Long> {
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
 }
